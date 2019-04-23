b68d34e304b99ba55e27b79cb0c605897da0423e
Ahmed Teleb <ahmedasd37@gmail.com>
2019-04-23 12:17:30 +0200

    Initial commit - add _ "splash screen and choose login & registration screen"

---
+++ app/src/main/java/com/ahmedteleb/requestchat/ChooseLoginRegistrationActivity.java
@@ -0,0 +1,41 @@
+package com.ahmedteleb.requestchat;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class ChooseLoginRegistrationActivity extends AppCompatActivity {
+
+    Button login_btn;
+    Button registration_btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choose_login_registration);
+
+        login_btn = findViewById(R.id.login_btn);
+        registration_btn = findViewById(R.id.registration_btn);
+
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent login_intent =new Intent(getApplication(),LoginActivity.class);
+                startActivity(login_intent);
+                return;
+            }
+        });
+
+        registration_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent register_intent =new Intent(getApplication(),RegistrationActivity.class);
+                startActivity(register_intent);
+                return;
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/ahmedteleb/requestchat/SplashScreenActivity.java
@@ -0,0 +1,37 @@
+package com.ahmedteleb.requestchat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SplashScreenActivity extends AppCompatActivity
+{
+    public  static boolean started = false;
+    private FirebaseAuth mAuth;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        if(mAuth.getCurrentUser() != null)
+        {
+            Intent main_intent =new Intent(getApplication(),MainActivity.class);
+            main_intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(main_intent);
+            return;
+
+        }else
+        {
+            Intent choose_intent =new Intent(getApplication(),ChooseLoginRegistrationActivity.class);
+            choose_intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(choose_intent);
+            return;
+        }
+    }
+}
