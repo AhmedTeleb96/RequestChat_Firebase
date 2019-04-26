eade1adb85c39f62b2321009c656e6dad664b7b8
Ahmed Teleb <ahmedasd37@gmail.com>
2019-04-27 01:10:51 +0200

    Add _ Camera preview

--- app/src/main/java/com/ahmedteleb/requestchat/CameraFragment.java
+++ app/src/main/java/com/ahmedteleb/requestchat/CameraFragment.java
@@ -1,16 +1,35 @@
 package com.ahmedteleb.requestchat;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.camera2.CameraDevice;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class CameraFragment extends Fragment {
+import com.google.firebase.auth.FirebaseAuth;
 
+import java.io.IOException;
+
+public class CameraFragment extends Fragment implements SurfaceHolder.Callback {
+
+    Camera camera;
+    SurfaceView surfaceView;
+    SurfaceHolder surfaceHolder;
+    final int CAMERA_REQUEST_CODE =1;
     public static CameraFragment getInstance_()
     {
         CameraFragment cameraFragment = new CameraFragment();
@@ -21,6 +40,86 @@ public class CameraFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_camera , container,false);
+
+        surfaceView = view.findViewById(R.id.surfaceView);
+        surfaceHolder = surfaceView.getHolder();
+
+        if(ActivityCompat.checkSelfPermission(getContext(), android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+        {
+           ActivityCompat.requestPermissions(getActivity(),new String[]{android.Manifest.permission.CAMERA}, CAMERA_REQUEST_CODE);
+        }else {
+            surfaceHolder.addCallback(this);
+            surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        }
+
+        Button logout = view.findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logout();
+            }
+        });
+
         return view;
+    }
+
+
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder2) {
+        camera = Camera.open();
+        Camera.Parameters param = camera.getParameters();
+        camera.setDisplayOrientation(90);
+        param.setPreviewFrameRate(30);
+        param.setFlashMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+
+        camera.setParameters(param);
+
+        try {
+            camera.setPreviewDisplay(surfaceHolder2);
+        } catch (IOException e) {
+
+        }
+        camera.startPreview();
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode)
+        {
+            case CAMERA_REQUEST_CODE :
+            {
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    surfaceHolder.addCallback(this);
+                    surfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+                }else {
+                    Toast.makeText(getContext(),"Please provide the permission",Toast.LENGTH_SHORT).show();
+                }
+
+                break;
+            }
+        }
+    }
+
+    private void logout() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(getContext(),SplashScreenActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+        return;
+
     }
 }
